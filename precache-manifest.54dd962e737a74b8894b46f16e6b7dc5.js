8f91d445229906e3fa907c6a3a6d6bf5fbf69650
Sutan Nasution <sutan.gnst@gmail.com>
2020-02-13 00:14:15 +0700

    8002c62

---
+++ precache-manifest.54dd962e737a74b8894b46f16e6b7dc5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c09a8bc06172cfa7cd64f89e15daee3",
+    "url": "/redirect/index.html"
+  },
+  {
+    "revision": "9d26d116e8e3833fcd5d",
+    "url": "/redirect/static/js/2.5a3422f3.chunk.js"
+  },
+  {
+    "revision": "c6f1b9eaf248765f73a1",
+    "url": "/redirect/static/js/main.1cf2b625.chunk.js"
+  },
+  {
+    "revision": "61ed77c7fcf11c1558ce",
+    "url": "/redirect/static/js/runtime~main.04f12f5a.js"
+  }
+]);
